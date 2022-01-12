7e98601188d3a7f7bb6ec62d286661c15dbb3731
zfoteff <zfoteff@zagmail.gonzaga.edu>
2022-01-11 18:27:37 -0800

    Created 5 initial tests

---
+++ hw1/test_count_words.py
@@ -0,0 +1,50 @@
+""" Main file for testing the HW1 implementation
+"""
+
+__version__ = '0.1'
+__author__ = 'Zac Foteff'
+
+import io
+from Loggers import TestLogger
+
+logger = TestLogger("hw1")
+
+TEST_FILES = [
+    'single_word.txt', 'long_word.txt', 'alpha.txt', 
+    '500_words.txt', 'every_paragraph.txt', 'empty.txt'
+]
+
+EXPECTED_OUTPUTS = {
+    'empty.txt': 0,
+    'single_word.txt': 1,
+    'long_word.txt': 1,
+    '500_words.txt': 500,
+    'every_paragraph': 10
+}
+
+def test_empty_file():
+    """ Test that an empty file returns 0 words
+    """
+    pass
+    
+def test_single_word():
+    """ Test that a single word returns 1 word
+    """
+    pass
+
+def test_long_word():
+    """ Test that an abnormally long word returns 1 word
+    """
+    pass
+
+def test_500_words_file():
+    """ Test that a file with 500 words returns 500 words
+    """
+    pass
+
+def test_same_expected_output():
+    """ 
+    Test that a 2 instances of the count_words function returns the same amount of words
+    on an file with an unknown amount of words
+    """
+    pass
