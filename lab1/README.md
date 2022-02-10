d4bcb8ff41ead3fd13fed8af96d7fea22ac2912c
zfoteff <zfoteff@zagmail.gonzaga.edu>
2022-02-10 13:49:45 -0800

    Changes

--- lab1/README.md
+++ lab1/README.md
@@ -1,10 +1,19 @@
-Uses point-to-point venv
+# Messaging System
 
-to run
+Uses point-to-point venv  
+To activate:
+
+```bash
+point-to-point/Scripts/Activate.ps1
+```
+
+To run:
 
 ```bash
 python -m uvicorn p2pchat:app --reload
 ```
 
 Messaging server running rabbitmq
-cps-devops
+cps-devops  
+Connection information forthcoming
+
--- lab1/p2pchat.py
+++ lab1/p2pchat.py
@@ -1,17 +1,30 @@
-from fastapi import FastAPI
-import requests
+from json import JSONDecodeError
+import webbrowser
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {'message': 'Hello, World!'}
-
-@app.post("/")
-async def put_root(message):
-    return {'message': message}
+async def root(req: Request, res: Response):
+    default_response = {'message': 'Hello, World!'}
+    
+    try:
+        body = await req.json()
+        if body.message == None:
+            return default_response
+        elif body.message == "":
+            return default_response
+        else:
+            res.
+            return {'message': body.message}
+    except JSONDecodeError:
+        return default_response
 
 @app.post("/send/")
-async def root(sender: str, message: str):
-    print (f"Sender: {sender}\nMessage: {message}")
-    return {"sender": sender, 'message': message}
+async def root(req: Request):
+    try:
+        body = await req.json()
+        webbrowser.open(body)
+        return body
+    except JSONDecodeError:
+        return {"Error": "Unexpected error trying to recieve message"}
