97ff870521df14236e00d5d82089d2b84fbf6cf7
zfoteff <zfoteff@zagmail.gonzaga.edu>
2022-02-20 20:31:37 -0800

    Finished lab 3, move these files to new github repo

--- hw3/point-to-point/Scripts/activate.bat
+++ hw3/point-to-point/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Zac\GitHub\cpsc313\lab1\point-to-point
+set VIRTUAL_ENV=C:\Users\Zac\GitHub\cpsc313\hw3\point-to-point
 
 if not defined PROMPT set PROMPT=$P$G
 
